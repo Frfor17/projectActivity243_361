8362caf96793fc1716c292066464dc58fc36185f
Фёдор Хлевицкий <brother.23@yandex.ru>
2025-01-28 21:33:43 +0300

    New data models
    Добавил новые модели данных

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class Users(Base):
     password = Column(String)
     image_path = Column(String)
 
-    pet_projects = relationship("PetProjects", back_populates="users")
+    # pet_projects = relationship("PetProjects", back_populates="users")
     likes = relationship("Likes", back_populates="users")
 
 
@@ -23,27 +23,27 @@ class Likes(Base):
     score = Column(Integer)
 
     users = relationship("Users", back_populates="likes")
-    pet_projects = relationship("PetProjects", back_populates="likes")
+    # pet_projects = relationship("PetProjects", back_populates="likes")
 
 
-class PetProjects(Base):
-    __tablename__ = "pet_projects"
-    project_id = Column(Integer, primary_key=True)  # Исправлено на primary_key
-    user_id = Column(Integer, ForeignKey("users.user_id"))
-    theme_id = Column(Integer, ForeignKey("themes.theme_id"))
-    title = Column(String)
-    short_description = Column(String)
-    description = Column(String)
-    average_score = Column(Float(precision=53))
+# class PetProjects(Base):
+#     __tablename__ = "pet_projects"
+#     project_id = Column(Integer, primary_key=True)  # Исправлено на primary_key
+#     user_id = Column(Integer, ForeignKey("users.user_id"))
+#     theme_id = Column(Integer, ForeignKey("themes.theme_id"))
+#     title = Column(String)
+#     short_description = Column(String)
+#     description = Column(String)
+#     average_score = Column(Float(precision=53))
 
-    users = relationship("Users", back_populates="pet_projects")
-    likes = relationship("Likes", back_populates="pet_projects")
-    themes = relationship("Themes", back_populates="pet_projects")
+#     users = relationship("Users", back_populates="pet_projects")
+#     likes = relationship("Likes", back_populates="pet_projects")
+#     themes = relationship("Themes", back_populates="pet_projects")
 
 
-class Themes(Base):
-    __tablename__ = "themes"
-    theme_id = Column(Integer, primary_key=True, index=True)
-    theme_name = Column(String)
+# class Themes(Base):
+#     __tablename__ = "themes"
+#     theme_id = Column(Integer, primary_key=True, index=True)
+#     theme_name = Column(String)
 
-    pet_projects = relationship("PetProjects", back_populates="themes")
+#     pet_projects = relationship("PetProjects", back_populates="themes")
