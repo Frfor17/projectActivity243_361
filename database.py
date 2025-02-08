34961991e7332730fb0bb670e7b5915ba3883bd7
Фёдор Хлевицкий <brother.23@yandex.ru>
2025-02-08 23:43:01 +0300

    First endpoint
    Добавил первый эндпоинт

--- database.py
+++ database.py
@@ -10,6 +10,3 @@ SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=F
 Base = declarative_base()
 
 
-async def get_db():
-    async with SessionLocal() as session:
-        yield session
--- main_auth.py
+++ main_auth.py
@@ -1,5 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from models import Users
 
 
 # Создаем приложение
 app = FastAPI()
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
+@app.get("/users")
+async def read_all_users(db: Session = Depends(get_db)):
+    users = db.query(Users).all()
+    return users
